cd34a5e4f2a1dce2b63d01387e748764844ef7b1
kumkum <kumkumrathour00@gmail.com>
2022-09-17 09:18:34 +0530

    the change

--- searchInRange.java
+++ searchInRange.java
@@ -2,6 +2,19 @@ package linear_search;
 
 public class searchInRange {
     public static void main(String[] args){
-        
+    int[] arr ={32,-58,43,66,21,78,-45};
+    int target = 43;
+    System.out.print(linearSearch(arr,target,2,5));
+    }
+    static int linearSearch(int[] arr, int target, int start, int end){
+        if(arr.length== 0){
+            return -1;
+        }
+        for(int i =start; i<=end ;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
     }
 }
